32efd2e2d720d40415ea3f59d201e675d332e9d1
achen236 <achen236@buffalo.edu>
2022-03-03 11:07:31 -0500

    save schedDict to csv and pickle

--- ub_sched_view.py
+++ ub_sched_view.py
@@ -1,8 +1,11 @@
-import sched
 import pandas as pd
-import ub_sched_scraper
+import pickle
 from os.path import exists
 
+import ub_sched_scraper
+
+
+# Get department schedule to csv
 def showDeptSchedule(schedDict, dept):
     # Create dataframe
     df = pd.DataFrame.from_records(schedDict[dept])
@@ -10,18 +13,31 @@ def showDeptSchedule(schedDict, dept):
     if not exists("./csv/" + dept + ".csv"):
         df.to_csv("./csv/" + dept + ".csv", index = False)
 
-def showSchoolSched():
+# Get all department schedules to csv
+def showSchoolSched(schedDict):
+    for dept, courseDict in schedDict.items():
+        showDeptSchedule(schedDict, dept)
+
+# Save schedDict to pickle
+def saveSchedDict():
     # Scrape and collect data
     # {"Dept": [{"ClassAttr": Value}]}
     schedDict = ub_sched_scraper.getSchedDict("spring")
 
-    for dept, courseDict in schedDict.items():
-        showDeptSchedule(schedDict, dept)
+    with open("schedDict.pkl", "wb") as tf:
+        pickle.dump(schedDict, tf)
 
+# Load schedDict from pickle
+def loadSchedDict():
+    with open("schedDict.pkl", "rb") as tf:
+        schedDict = pickle.load(tf)
+    return schedDict
 
 def main():
     #showDeptSchedule("CSE")
-    showSchoolSched()
+    #showSchoolSched()
+    #saveSchedDict()
+    print(loadSchedDict())
     
 if __name__ == "__main__":
     main()
