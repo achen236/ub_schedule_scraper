2d62f674fdf6f359d5b008d7fafb5608644de700
achen236 <achen236@buffalo.edu>
2022-03-23 00:19:59 -0400

    added parseDays

--- ub_sched_plot.py
+++ ub_sched_plot.py
@@ -4,26 +4,54 @@ import ub_sched_data, ub_sched_scraper
 import plotly.express as px
 
 # return matrix with values of number of people in classes at an hour of day
-def schedPeopleMatrix(timeRange: int):
-    matrix = initializeSchedMatrix(timeRange)
+def schedPeopleMatrix(timeRange: int, dayRange:int):
+    # initialize matrix of zeros
+    matrix = initializeSchedMatrix(timeRange, dayRange)
     schedDict = ub_sched_scraper.loadSchedDict()
     for dept in schedDict.keys():
         for course in dept:
+            days = course["Days"]
             time = course["Time"]
+            # get binary list of class days
+            binDays = parseDays(days)
+            # get tuple (start,end) in military time
             startEndMilTime = parseTime(time)
-            
+        for i in range(dayRange):
+            if binDays[i] == 1:
+                for 
 
     return data
 
-# [[ ... ], [ ... ]. [ ... ] ]
-def initializeSchedMatrix(timeRange: int):
+def getListofHours(timeInterval):
+    
+
+
+# [[ 0 ], [ 1 ]. [ 2 ], ... [ timeRange ] ]
+def initializeSchedMatrix(timeRange: int, dayRange: int):
     retMatrix = []
     for hour in range(timeRange):
         row = []
-        for day in range(5):
-            row.append(0)
-        retMatrix.append(row)
+        retMatrix.append(listOfZeros(dayRange))
     return retMatrix
+
+# returns binary list. 1 if class else 0 
+def parseDays(days: String):
+    retList = listOfZeros(6)
+    days = ub_sched_scraper.removeAllWhiteSpace(days)
+    for day in days:
+        if day == "M":
+            retList[0] == 1
+        elif day == "T":
+            retList[1] == 1
+        elif day == "W":
+            retList[2] == 1
+        elif day == "R":
+            retList[3] == 1
+        elif day == "F":
+            retList[4] == 1
+        elif day == "S":
+            retList[5] == 1
+    return retList
 
 # convert time to military time
 # return tuple start end time of class
@@ -50,7 +78,9 @@ def militaryTime(time: String):
         if timeAM >= 1200:
             timeAM = timeAM - 1200
         return timeAM
-        
+
+def listOfZeros(n: int):
+    return [0] * n
 
 def main():
     return 0
